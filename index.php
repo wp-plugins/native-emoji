18b1a7f62f04f67ec31cd8c177ef8917f6e97d69
Andrew Stolarchuk <barbocc@gmail.com>
2017-01-05 14:01:57 +1000

    Changed admin_head, to just wp_hook. That allows add emoji on frontend mce_editor

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ class WP_nep_Native_Emoji{
 	function __construct() {
 		add_action( "wp_head", array( $this, 'nep_emoji_localize_tinymce_javascript' ));
 		wp_enqueue_style( 'nep_native-emoji' );
-//		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'nep_native-emoji' );
 		// Add the actions to the hooks
 		foreach ( array('post.php','post-new.php') as $hook ) {
